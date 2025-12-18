8255b17b9fe19aa4c27d7a1ae820613930044203
themirza001 <themirza001@gmail.com>
2025-12-18 17:51:51 +0530

    check 3

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -5,15 +5,20 @@ resource "aws_db_subnet_group" "db_subnets" {
 
 resource "aws_db_instance" "rds" {
   allocated_storage      = 20
-  engine                 = "postgres"
-  engine_version         = "15"
+  engine                 = "mysql"
+  engine_version         = "8.0"
   instance_class         = "db.t3.micro"
+
   db_name                = "appdb"
   username               = "mirza"
   password               = "mirza123"
+
   skip_final_snapshot    = true
   publicly_accessible    = false
 
   db_subnet_group_name   = aws_db_subnet_group.db_subnets.name
   vpc_security_group_ids = [aws_security_group.rds_sg.id]
+
+  backup_retention_period = 7
+  deletion_protection     = false
 }
--- terraform/sg.tf
+++ terraform/sg.tf
@@ -45,13 +45,15 @@ resource "aws_security_group" "ecs_sg" {
 # --------------------
 # RDS Security Group
 # --------------------
+
+
 resource "aws_security_group" "rds_sg" {
   name   = "rds-sg"
   vpc_id = aws_vpc.main.id
 
   ingress {
-    from_port       = 5432
-    to_port         = 5432
+    from_port       = 3306
+    to_port         = 3306
     protocol        = "tcp"
     security_groups = [aws_security_group.ecs_sg.id]
   }
